f382476ee259e61812a224d6276e1a8a136e38ae
Murat Artan <muratartan@hotmail.com>
2024-05-23 10:23:43 +0300

    set vote program

--- Class_code/vote/programs/vote/src/lib.rs
+++ Class_code/vote/programs/vote/src/lib.rs
@@ -6,7 +6,15 @@ declare_id!("4kbqKAWg1wg4t7UynAVnCEmiC2eonUxZm8c61xMPR77D");
 pub mod vote {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, _url: String) -> Result<()> {
+        ctx.accounts.initialize();
+    }
+
+    pub fn upvote(ctx: Context<Vote>, _url: String) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn downvote(ctx: Context<Vote>, _url: String) -> Result<()> {
         Ok(())
     }
 }
