c28444f146ecd607d7ed4943ddd3c90003515742
Murat Artan <muratartan@hotmail.com>
2024-05-23 10:54:52 +0300

    set accounts for initialize instruction

--- Class_code/vote/programs/vote/src/lib.rs
+++ Class_code/vote/programs/vote/src/lib.rs
@@ -20,4 +20,36 @@ pub mod vote {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(_url: String)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(init,
+        payer = payer,
+        seeds = [_url.as_bytes().as_ref()],
+        bump,
+        space = VoteState::INIT_SPACE
+    )]
+    pub vote_account: Account<'info, VoteState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vote_account.score = 0;
+        self.vote_account.bump = bumps.vote_account;
+
+        Ok(())
+    }
+}
+
+
+#[account]
+pub struct VoteState {
+    pub score: i64,
+    pub bump: u8,
+}
+
+impl Space for VoteState {
+    const INIT_SPACE: usize = 8 + 8 + 1;
+}
