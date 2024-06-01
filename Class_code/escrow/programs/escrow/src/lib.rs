b28a3c5b3c124b6af2953f00a75b2433e56c36d1
Murat Artan <muratartan@hotmail.com>
2024-06-01 10:52:44 +0300

    create escrow project

---
+++ Class_code/escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("2ZCEiywq7CqqiNo726wDeX9croaZwBxwtExGwf4dg7s7");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
