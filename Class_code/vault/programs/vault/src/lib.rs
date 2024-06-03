8d489fd884123a20fcac4e089475822fc1f575b1
Murat Artan <muratartan@hotmail.com>
2024-06-03 10:13:50 +0300

    set vault program functions

--- Class_code/vault/programs/vault/src/lib.rs
+++ Class_code/vault/programs/vault/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
 
 declare_id!("DGzACNZ7YcnLEA8B7ZtJPt25bxXi95KXxrQXCLwvXARn");
 
@@ -7,8 +7,29 @@ pub mod vault {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize(&ctx.bumps)?;
+
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)?;
+
+        Ok(())
+    }
+
+    pub fn close(ctx: Context<close>) -> Result<()> {
+        ctx.accounts.close()?;
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
