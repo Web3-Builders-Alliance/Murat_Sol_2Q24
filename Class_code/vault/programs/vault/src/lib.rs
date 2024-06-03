e0b8236248a21f2d7bff0fe503f6325a5c84f8ab
Murat Artan <muratartan@hotmail.com>
2024-06-03 10:31:22 +0300

    set initialize instruction

--- Class_code/vault/programs/vault/src/lib.rs
+++ Class_code/vault/programs/vault/src/lib.rs
@@ -32,5 +32,48 @@ pub mod vault {
 
 }
 
+// ---------------------------------------
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"state", user.key().as_ref()],
+        bump,
+        space = VaultState::INIT_SPACE,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vault_state.vault_bump = bumps.vault;
+        self.vault_state.state_bump = bumps.vault_state;
+
+        Ok(())
+    }
+}
+
+
+
+
+// ----------------------------------------
+
+#[account]
+pub struct VaultState {
+    pub vault_bump: u8,
+    pub state_bump: u8,
+}
+
+impl Space for VaultState {
+    const INIT_SPACE: usize = 8 + 1 + 1;
+}
