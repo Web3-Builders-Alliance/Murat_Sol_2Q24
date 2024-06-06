e07d28eced4e26c53f184191463f350c44ac7262
Murat Artan <muratartan@hotmail.com>
2024-06-06 13:34:27 +0300

    impl make

--- Class_code/escrow/programs/escrow/src/contexts/make.rs
+++ Class_code/escrow/programs/escrow/src/contexts/make.rs
@@ -23,7 +23,7 @@ pub struct Make<'info> {
         associated_token::mint = mint_a,
         associated_token::authority = maker,
     )]
-    pub maker_ata: Account<'info, TokenAccount>
+    pub maker_ata: Account<'info, TokenAccount>,
     #[account(
         init,
         payer = maker,
@@ -34,4 +34,35 @@ pub struct Make<'info> {
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>
+}
+
+impl<'info> Make<'info> {
+    pub fn init(&mut self, seed: u64, receive: u64, bumps: &MakeBumps) -> Result<()> {
+        self.escrow.set_inner(Escrow { 
+            seed, 
+            maker: self.maker.key(), 
+            mint_a: self.mint_a.key(), 
+            mint_b: self.mint_b.key(), 
+            receive, 
+            bump: bumps.escrow 
+        });
+
+        Ok(())
+    }
+
+    pub fn deposit(&mut self, deposit: u64) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.maker_ata.to_account_info(),
+            to: self.vault.to_account_info(),
+            authority: self.maker.to_account_info()
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, deposit)?;
+
+        Ok(())
+    }
 }
