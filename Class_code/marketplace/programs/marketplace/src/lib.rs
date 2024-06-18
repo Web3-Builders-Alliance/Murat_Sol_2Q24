2779f6a5cfbed9842c3071f64fe1f27df8dda7b5
Murat Artan <muratartan@hotmail.com>
2024-06-18 15:58:03 +0300

    set lib.rs

--- Class_code/marketplace/programs/marketplace/src/lib.rs
+++ Class_code/marketplace/programs/marketplace/src/lib.rs
@@ -6,14 +6,41 @@ mod state;
 mod error;
 mod contexts;
 
+use contexts::*;
+use error::*;
+
 #[program]
 pub mod marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+
         Ok(())
     }
+
+    pub fn listing(ctx: Context<List>, price: u64) -> Result<()> {
+
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;
+
+        Ok(())
+    }
+
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()?;
+
+        Ok(())
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_nft()?;
+        ctx.accounts.close_mint_vault()?;
+
+        Ok(())
+    }
+
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
